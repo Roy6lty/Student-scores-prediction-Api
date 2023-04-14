dc28f273a512bee58630e81f76dde5415bdc9deb
roy6lty <olowoleru06@gmail.com>
2023-04-14 07:10:30 +0100

    hyperparamter_tunining

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,9 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+from tqdm import tqdm
 from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -23,15 +24,20 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report={}
         logging.info("Evaluating_model_contin")
         
-        for i in range(len(list(models))):
+        for i in tqdm(range(len(list(models)))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) #Train model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
 
