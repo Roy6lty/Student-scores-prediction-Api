1d608861cbecd8d8dab46c304d7cb5dc42ebe17b
roy6lty <olowoleru06@gmail.com>
2023-04-18 11:39:53 +0100

    web_deveopment

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 
 import pandas as pd
 import numpy as np
@@ -58,14 +59,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
 def load_object(file_path):
     try:
-        with open(file_path,'rb') as file_obj:
-            return dill.load(file_obj)
-        
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
-
-
-
+        raise CustomException(e, sys)
 
 
 
